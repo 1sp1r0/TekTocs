640501b3bc15a9ed369cbb7b2c9636c1dc73c900
Murali Narasimhan <murali@agateconsulting.com>
2016-02-04 12:50:52 -0500

    socket.io successful connection between slack, nodejs and browser client

--- server/config.js
+++ server/config.js
@@ -1,3 +1,4 @@
 export default {
-    slackBotUserToken:process.env.OBVUIS_SLACK_BOT_USER_TOKEN || 'xoxb-17952512917-xCZUmeEjTLLjR3DtXqAkLv2v',
+    slackBotUserToken:process.env.OBVUIS_SLACK_BOT_USER_TOKEN || 'xoxb-17952512917-xCZUmeEjTLLjR3DtXqAkLv2v'
+    
 };
