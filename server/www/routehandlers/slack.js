6c6fbc60ec340ef6a7603c32f4b7eec972301958
Murali Narasimhan <murali@agateconsulting.com>
2016-02-05 18:45:18 -0500

    now using co, yield and promises

--- server/www/routehandlers/slack.js
+++ server/www/routehandlers/slack.js
@@ -22,7 +22,7 @@ export function oauth(req, res) {
                         res.sendStatus(500);
                     }
                 } catch (err) {
-                    winston.log('error', err.message);
+                    winston.log('error', err);
                     res.sendStatus(500);
                 }
             }).catch((err) => {
