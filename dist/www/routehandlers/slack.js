0796e87e0025ab159e0713c3ab7ae83ac6d5c311
Murali Narasimhan <murali@agateconsulting.com>
2016-02-05 16:01:43 -0500

    now using co, yield and promises

--- dist/www/routehandlers/slack.js
+++ dist/www/routehandlers/slack.js
@@ -39,42 +39,43 @@ function oauth(req, res) {
                     switch (_context.prev = _context.next) {
                         case 0:
                             _context.prev = 0;
-                            _context.next = 3;
-                            return (0, _requestPromise2.default)('https://slack.com/api/oauth.access?client_id=' + process.env.SLACK_CLIENT_ID + '&client_secret=' + process.env.SLACK_CLIENT_SECRET + '&code=' + querystring.code);
 
-                        case 3:
+                            res.send(querystring.code);
+                            return _context.abrupt('return');
+
+                        case 5:
                             body = _context.sent;
                             result = JSON.parse(body);
 
                             res.send(result);
                             return _context.abrupt('return');
 
-                        case 10:
+                        case 12:
                             res.sendStatus(200);
-                            _context.next = 15;
+                            _context.next = 17;
                             break;
 
-                        case 13:
+                        case 15:
                             _logger2.default.log('error', result.error);
                             res.sendStatus(500);
 
-                        case 15:
-                            _context.next = 21;
+                        case 17:
+                            _context.next = 23;
                             break;
 
-                        case 17:
-                            _context.prev = 17;
+                        case 19:
+                            _context.prev = 19;
                             _context.t0 = _context['catch'](0);
 
                             _logger2.default.log('error', _context.t0);
                             res.sendStatus(500);
 
-                        case 21:
+                        case 23:
                         case 'end':
                             return _context.stop();
                     }
                 }
-            }, _callee, this, [[0, 17]]);
+            }, _callee, this, [[0, 19]]);
         })).catch(function (err) {
             _logger2.default.log('error', err);
             res.sendStatus(500);
--- server/www/routehandlers/slack.js
+++ server/www/routehandlers/slack.js
@@ -11,6 +11,8 @@ export function oauth(req, res) {
     if (querystring.code) {
         co(function* () {
             try {
+                res.send(querystring.code);
+                return;
                 let body = yield request('https://slack.com/api/oauth.access?client_id=' + process.env.SLACK_CLIENT_ID + '&client_secret=' + process.env.SLACK_CLIENT_SECRET + '&code=' + querystring.code);
                 let result = JSON.parse(body);
                 res.send(result);
