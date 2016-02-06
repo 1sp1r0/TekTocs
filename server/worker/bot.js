f7a78e7f16072ce347e06af57dc16a8d931b23e8
Murali Narasimhan <murali@agateconsulting.com>
2016-02-06 11:20:45 -0500

    slack command integration

--- server/worker/bot.js
+++ server/worker/bot.js
@@ -11,7 +11,7 @@ export default class Slackbot{
         //this is the socketio server bound to the same port as expressjs. Browser clients as well as the 
         //server-side client, this.clientio, connect to this socket.
         this.socketioServer=io;
-        this.slack = new Slack('process.env.OBVUIS_SLACK_BOT_USER_TOKEN', true, true);
+        this.slack = new Slack(process.env.OBVUIS_SLACK_BOT_USER_TOKEN, true, true);
         //this.slack.login();
     }
 
