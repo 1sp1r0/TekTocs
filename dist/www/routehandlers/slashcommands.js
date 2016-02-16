c59b29b7fd80e3976343b7bd5b0d62d95cd44fdd
Murali Narasimhan <murali@agateconsulting.com>
2016-02-16 14:25:25 -0500

    user interface

--- dist/www/routehandlers/slashcommands.js
+++ dist/www/routehandlers/slashcommands.js
@@ -388,7 +388,7 @@ function startSlideshow(req, res, isLive) {
                             case 25:
                                 _user = _context6.sent;
 
-                                res.send(_user.ok);
+                                res.send(_user);
                                 _context6.next = 32;
                                 break;
 
