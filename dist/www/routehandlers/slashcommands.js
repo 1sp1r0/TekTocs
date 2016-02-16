76dc97afe616e8a13eabfb3304730df1c96c67a3
Murali Narasimhan <murali@agateconsulting.com>
2016-02-16 04:02:37 -0500

    user interface

--- dist/www/routehandlers/slashcommands.js
+++ dist/www/routehandlers/slashcommands.js
@@ -104,7 +104,7 @@ function publish(req, res) {
                                 messages = msgResponse.messages;
                                 slideIndex = 1;
 
-                                res.status(200).send(Array.from(messages).length);
+                                res.status(200).send(messages[0]);
                                 return _context.abrupt('return');
 
                             case 26:
